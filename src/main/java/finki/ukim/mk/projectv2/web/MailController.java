841ffc13abb6a1d0f4b5f32fbb7c56ef83c3d4e2
Predrag Spasovski <101633327+predrag1998@users.noreply.github.com>
2022-04-20 17:07:34 +0200

    checkbox and button for checked

--- src/main/java/finki/ukim/mk/projectv2/web/MailController.java
+++ src/main/java/finki/ukim/mk/projectv2/web/MailController.java
@@ -1,13 +1,16 @@
 package finki.ukim.mk.projectv2.web;
 
 import finki.ukim.mk.projectv2.model.Person;
+import finki.ukim.mk.projectv2.service.ApplicationService;
 import finki.ukim.mk.projectv2.service.impl.EmailServiceImpl;
 import finki.ukim.mk.projectv2.service.PersonService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.mail.MessagingException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -15,10 +18,12 @@ public class MailController {
 
     private final EmailServiceImpl emailServiceImpl;
     private final PersonService personService;
+    private final ApplicationService applicationService;
 
-    public MailController(EmailServiceImpl emailServiceImpl, PersonService personService) {
+    public MailController(EmailServiceImpl emailServiceImpl, PersonService personService, ApplicationService applicationService) {
         this.emailServiceImpl = emailServiceImpl;
         this.personService = personService;
+        this.applicationService = applicationService;
     }
 
     @GetMapping("/send/{mail}")
@@ -44,8 +49,16 @@ public class MailController {
         return "redirect:/showApplications";
     }
     @GetMapping("/send/all")
-    public String sendMailToAll()  {
-        List<Person> persons=personService.findAll();
+    public String sendMailToAll(@RequestParam(required = false)String[] allMail)  {
+        List<Person> persons=new ArrayList<>();
+        if(allMail.equals(null) || allMail.length==0){
+            persons=personService.findAll();
+        }else{
+            for(String s :allMail){
+                persons.add(applicationService.findById(Long.parseLong(s)).get().getPerson());
+            }
+        }
+
         for (Person p :
                 persons) {
             this.emailServiceImpl.sendSimpleMessage(p.getMail(),"Recruitment process(WP-project)", "Hello "+p.getName()+
